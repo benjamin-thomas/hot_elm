11c069e8aa44cab2d85ca2a7ff63627d6a8f3bb1
Benjamin <benjamin.guy.thomas@gmail.com>
2022-08-06 21:40:56 +0200

    Buggy dev, try in prod...

--- app/javascript/controllers/elm_init_controller.js
+++ app/javascript/controllers/elm_init_controller.js
@@ -4,6 +4,16 @@ import {Controller} from "@hotwired/stimulus"
 // Connects to data-controller="elm-init"
 export default class extends Controller {
     connect() {
-        Elm.Main.init({ node: this.element });
+        console.log(new Date(), "Connecting Elm node");
+        if (this.init === undefined) {
+            console.log(new Date(), "Initializing Elm node");
+            this.init = Elm.Main.init({node: this.element});
+        } else {
+            console.log(new Date(), "Reusing Elm node");
+        }
+    }
+
+    disconnect() {
+        // this.element.remove();
     }
 }
